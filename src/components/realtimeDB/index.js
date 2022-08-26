f612509010cdbce7ed055d5c5e4edb12914f390a
anuragshukla07 <anuragshukla07official@gmail.com>
2022-08-26 20:25:50 +0530

    basicDone

--- src/components/realtimeDB/index.js
+++ src/components/realtimeDB/index.js
@@ -1,3 +1,49 @@
 import StartFirebase from "../firebaseconfig/index";
 import React from "react";
 import { ref,onValue } from "firebase/database";
+import { Table } from "react-bootstrap";
+
+const db = StartFirebase();
+
+export class realtimeData extends React.Component{
+    constructor(){
+         super();
+         this.state = {
+            tableData: []
+         }
+    }
+
+    componentDidMount(){
+        const dbRef = ref(db,'Customer');
+
+        onValue(dbRef ,(snapshot)=>{
+
+        });
+    }
+
+
+    render(){
+        return(
+        <Table>
+            <thead>
+                <tr>
+                    <th>Received Location</th>
+                    <th>Received Photo</th>
+                </tr>
+            </thead>
+            <tbody>
+                {this.state.tableData.map((row,index)=>{
+                    return(
+                        <tr>
+                        <td>{index}</td>
+                        <td>{row.key}</td>
+                        <td>{row.data.latitude}</td>
+                        <td>{row.data.longitude}</td>
+                    </tr>
+                    )
+                })}
+            </tbody>
+        </Table>
+        )   
+    }
+}
